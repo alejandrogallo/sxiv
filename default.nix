fc42484ea40e88b303458eea5e2ccedd7c173197
Alejandro Gallo <aamsgallo@gmail.com>
2020-09-10 00:34:08 +0200

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> {};
+let
+  buildInputs = [
+    binutils
+    zlib
+    xorg.libX11
+    xorg.libXft
+    xorg.libXinerama
+    libexif
+    imlib2
+    giflib
+  ];
+in
+stdenv.mkDerivation {
+  system = builtins.currentSystem;
+  buildInputs = buildInputs;
+  name = "sxiv";
+  src = ./.;
+  makeFlags = [ "CC:=$(CC)" ];
+  preConfigure = ''
+    sed -i "s@PREFIX = /usr/local@PREFIX = $out@g" Makefile
+  '';
+}
